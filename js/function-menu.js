b988aeb9b0f62cc1ed2978eb59aa7760a3141d33
Nicole Marques <61321129+nicolembecker@users.noreply.github.com>
2020-07-14 21:33:59 -0300

    Add files via upload

---
+++ js/function-menu.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    // Eu quero abrir a pagina list-disciplinas.html dentro do index.html
+
+    $('#conteudo').load('src/disciplinas/visao/list-disciplinas.html')
+})
