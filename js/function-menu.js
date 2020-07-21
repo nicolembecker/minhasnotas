f9f1345e2d056b033d65d5a7ff985b7293b398f1
Nicole Marques <61321129+nicolembecker@users.noreply.github.com>
2020-07-21 19:07:56 -0300

    atualizacao

--- js/function-menu.js
+++ js/function-menu.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
-    // Eu quero abrir a pagina list-disciplinas.html dentro do index.html
 
-    $('#conteudo').load('src/disciplinas/visao/list-disciplinas.html')
+    $('#adiciona').click(function(e) {
+        e.preventDefault()
+        $('#conteudo').empty()
+        $('#conteudo').load('src/disciplinas/visao/adiciona-disciplinas.html')
+    })
+
 })
